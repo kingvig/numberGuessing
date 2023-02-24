437c1f8e00155f90cf295316cfddd9b9a4c5497a
kingvig <vignydjoumessi@gmail.com>
2023-02-24 12:54:42 +0100

    second commit avec les fonctionalites de comparaison du nombre genere avec le nombre entre, puis affichage du score

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 'use strict';
-console.log(document.querySelector('.message').textContent );
-document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.check').addEventListener('click',check);
+const numberToGuess =Math.trunc( Math.random()*20)+1;
+let  score = 20;
+function check(){
+    const guess = Number(document.querySelector('.guess').value);
+    
+
+    document.querySelector('.number').textContent = guess;
+    if(!guess){
+        document.querySelector('.message').textContent = '😂😂😂Please enter a number';
+    }
+    else if(guess==numberToGuess){
+        document.querySelector('.message').textContent =
+          'you guessed the number';
+    }
+    else if(guess<numberToGuess){
+        document.querySelector('.message').textContent =
+          'you are too low';
+          score -=1;
+          document.querySelector('.score').textContent = score;
+    }
+    else if(guess>numberToGuess){
+        document.querySelector('.message').textContent = 'you are too high';
+        score -=1;
+        document.querySelector('.score').textContent = score;
+    }
+}
