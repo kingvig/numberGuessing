8c5f1fe191714c21f0fe09c3f68f4785cd923965
kingvig <vignydjoumessi@gmail.com>
2023-02-24 13:34:54 +0100

    'troisieme commit avec les fonctionalites de changement de couleur pour la victoire et pour reset le jeu une fois qu'on atteint le score de 0'

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 document.querySelector('.check').addEventListener('click',check);
-const numberToGuess =Math.trunc( Math.random()*20)+1;
+let  numberToGuess =Math.trunc( Math.random()*20)+1;
 let  score = 20;
+let highScore = 0;
 function check(){
     const guess = Number(document.querySelector('.guess').value);
     
@@ -10,20 +11,49 @@ function check(){
     document.querySelector('.number').textContent = guess;
     if(!guess){
         document.querySelector('.message').textContent = '😂😂😂Please enter a number';
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
     }
     else if(guess==numberToGuess){
+        
         document.querySelector('.message').textContent =
           'you guessed the number';
+          document.querySelector('body').style.backgroundColor='#60b347';
+          document.querySelector('.number').style.width='30rem';
     }
     else if(guess<numberToGuess){
-        document.querySelector('.message').textContent =
-          'you are too low';
-          score -=1;
-          document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'you are low';
+            score -= 1;
+            document.querySelector('.score').textContent = score;
+
+        }
+        else{
+            document.querySelector('.message').textContent = 'You loose try again later';
+            
+        }
+        
     }
     else if(guess>numberToGuess){
-        document.querySelector('.message').textContent = 'you are too high';
-        score -=1;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+          document.querySelector('.message').textContent = 'you are high';
+          score -= 1;
+          document.querySelector('.score').textContent = score;
+        } else {
+          document.querySelector('.message').textContent =
+            'You loose try again later';
+        }
     }
+}
+document.querySelector('.again').addEventListener('click', reset);
+
+function reset(){
+    score = 20;
+    numberToGuess = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
 }
